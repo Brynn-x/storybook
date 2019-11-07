1e40cdb535c1c37b22b6a2e3bb1084a23f990580
Brynn-X <ximengjieaa@gmail.com>
2019-11-07 17:11:10 +0800

    Simple component

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,15 @@
-import { configure } from '@storybook/react';
+// .storybook/config.js
 
-// automatically import all files ending in *.stories.js
-configure(require.context('../src/stories', true, /\.stories\.js$/), module);
+import { configure } from '@storybook/react';
+import requireContext from 'require-context.macro';
+import '../src/index.css';
+
+const req = requireContext('../src/components', true, /\.stories.js$/);
+
+// console.log(req.keys())
+//  不支持 CSF
+function loadStories() {
+  req.keys().forEach(filename => req(filename));
+}
+
+configure(loadStories, module);
